3276e16aa713690860e4e4bd3cbd93df8ca8ab7c
Ryan Northey <ryan@space23.net>
2015-09-28 12:13:29 +0100

    fixing migration dep

--- pootle_vcs/migrations/0001_initial.py
+++ pootle_vcs/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pootle_project', '0002_auto_20150923_1715'),
+        ('pootle_project', '0001_initial'),
     ]
 
     operations = [
